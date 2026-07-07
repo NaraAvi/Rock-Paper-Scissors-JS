4d52afa6c73ade915e05a5617c99e25a02d98650
Naravi <170275956+NaraAvi@users.noreply.github.com>
2026-07-07 22:47:23 +0200

    Added Buttons with Event Listeners added buttons with event listeners to parse chosen buttons value to PlayRound() method

--- game.js
+++ game.js
@@ -49,51 +49,58 @@ function DecideOutcome(humanChoice, computerChoice) {
   return "tie";
 }
 
-function PlayRound() {
+const PlayRound = (humanChoice) => {
   gamesPlayed++;
-  let humanChoice = getHumanChoice();
+  //let humanChoice = getHumanChoice();
   let computerChoice = getComputerChoice();
 
-  alert(`Computer Chooses: ${computerChoice}`);
-
+  //alert(`Computer Chooses: ${computerChoice}`);
+  console.log(humanChoice);
   switch (DecideOutcome(humanChoice, computerChoice)) {
     case "tie":
       humanScore++;
       computerScore++;
-      alert(`It is a Tie: Human: ${humanScore} || Computer: ${computerScore}`);
+      //alert(`It is a Tie: Human: ${humanScore} || Computer: ${computerScore}`);
       break;
     case "human":
       humanScore++;
-      alert(`Human Wins! Human: ${humanScore} || Computer: ${computerScore}`);
+      //alert(`Human Wins! Human: ${humanScore} || Computer: ${computerScore}`);
       break;
     case "computer":
       computerScore++;
-      alert(`Computer Wins! Human: ${humanScore} || Computer: ${computerScore}`);
+      //alert(`Computer Wins! Human: ${humanScore} || Computer: ${computerScore}`);
       break;
   }
 }
 
 function PlayGame() {
-  if (gamesPlayed >= 5) {
-    if (humanScore === computerScore) {
-      alert(`Wow, It is a tie! ${humanScore} || Computer: ${computerScore}`);
-    } 
-    else {
-      switch (humanScore > computerScore) {
-        case true:
-          alert(`Congratulations, You win the game!!!! ${humanScore} || Computer: ${computerScore}`);
-          break;
-        case false:
-          alert(`Damn, You lost the game! Better luck next time Human ${humanScore} || Computer: ${computerScore}`);
-          break;
-      }
-    }
-    humanScore = 0;
-    computerScore = 0;
-    gamesPlayed = 0;
-  }
-
-  PlayRound();
+  // if (gamesPlayed >= 5) {
+  //   if (humanScore === computerScore) {
+  //     alert(`Wow, It is a tie! ${humanScore} || Computer: ${computerScore}`);
+  //   } 
+  //   else {
+  //     switch (humanScore > computerScore) {
+  //       case true:
+  //         alert(`Congratulations, You win the game!!!! ${humanScore} || Computer: ${computerScore}`);
+  //         break;
+  //       case false:
+  //         alert(`Damn, You lost the game! Better luck next time Human ${humanScore} || Computer: ${computerScore}`);
+  //         break;
+  //     }
+  //   }
+  //   humanScore = 0;
+  //   computerScore = 0;
+  //   gamesPlayed = 0;
+  // }
+  // PlayRound()
+  const playerChoices = document.querySelectorAll("#player-choice > button");
+  // console.log(playerChoices.values);
+  playerChoices.forEach((elem) => {
+    elem.addEventListener("click", (event) => {
+      PlayRound(event.target.value)
+    })
+  });
+  
 }
 
 PlayGame();
