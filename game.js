6aafa04bb6b422e4536571f3e0948b377d56832a
Naravi <170275956+NaraAvi@users.noreply.github.com>
2026-07-07 22:15:51 +0200

    Refactor of game.js Adjusted source code for readability

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ let gamesPlayed = 0;
 function getComputerChoice() {
   const MIN = 1;
   const MAX = 3;
+
   //Choose a number between the min and max values, max inclusive. Note that Math.random() itself only returns values between 0 and 0.9999999...
   const choicenum = Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
 
@@ -22,120 +23,77 @@ const getHumanChoice = () =>
   prompt("Choose One: rock || paper || scissors", "rock").toLowerCase();
 
 function DecideOutcome(humanChoice, computerChoice) {
-  let outcome = "";
-
-  if (humanChoice === computerChoice) {
-    outcome = "tie";
-  }
 
   if (humanChoice === "rock") {
     switch (computerChoice) {
       case "paper":
-        outcome = "computer";
-        break;
+        return "computer";
       case "scissors":
-        outcome = "human";
-        break;
+        return "human";
     }
   } else if (humanChoice === "scissors") {
     switch (computerChoice) {
       case "rock":
-        outcome = "computer";
-        break;
+        return "computer";
       case "paper":
-        outcome = "human";
-        break;
+        return "human";
     }
   } else if (humanChoice === "paper") {
     switch (computerChoice) {
       case "scissors":
-        outcome = "computer";
-        break;
+        return "computer";
       case "rock":
-        outcome = "human";
-        break;
+        return "human";
     }
   }
-  return outcome;
+  return "tie";
+}
+
+function PlayRound() {
+  gamesPlayed++;
+  let humanChoice = getHumanChoice();
+  let computerChoice = getComputerChoice();
+
+  alert(`Computer Chooses: ${computerChoice}`);
+
+  switch (DecideOutcome(humanChoice, computerChoice)) {
+    case "tie":
+      humanScore++;
+      computerScore++;
+      alert(`It is a Tie: Human: ${humanScore} || Computer: ${computerScore}`);
+      break;
+    case "human":
+      humanScore++;
+      alert(`Human Wins! Human: ${humanScore} || Computer: ${computerScore}`);
+      break;
+    case "computer":
+      computerScore++;
+      alert(`Computer Wins! Human: ${humanScore} || Computer: ${computerScore}`);
+      break;
+  }
 }
 
 function PlayGame() {
-  console.log(`Alright, Lets Play`);
-
-  function PlayRound() {
-    gamesPlayed++;
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-
-    console.log(`Your Choice: ${humanChoice}`);
-    console.log(`Computers Choice: ${computerChoice}`);
-    alert(`Computer Chooses: ${computerChoice}`);
-
-    let outcome = DecideOutcome(humanChoice, computerChoice);
-
-    switch (outcome) {
-      case "tie":
-        (humanScore++, computerScore++);
-        console.log(
-          `It is a Tie: Human: ${humanScore} || Computer: ${computerScore}`,
-        );
-        alert(
-          `It is a Tie: Human: ${humanScore} || Computer: ${computerScore}`,
-        );
-        break;
-      case "human":
-        humanScore++;
-        console.log(
-          `Human Wins! Human: ${humanScore} || Computer: ${computerScore}`,
-        );
-        alert(`Human Wins! Human: ${humanScore} || Computer: ${computerScore}`);
-        break;
-      case "computer":
-        computerScore++;
-        console.log(
-          `Computer Wins! Human: ${humanScore} || Computer: ${computerScore}`,
-        );
-        alert(
-          `Computer Wins! Human: ${humanScore} || Computer: ${computerScore}`,
-        );
-        break;
-    }
-    PlayGame();
-  }
-
   if (gamesPlayed >= 5) {
     if (humanScore === computerScore) {
-      console.log(
-        `Wow, It is a tie! ${humanScore} || Computer: ${computerScore} `,
-      );
       alert(`Wow, It is a tie! ${humanScore} || Computer: ${computerScore}`);
-    } else {
+    } 
+    else {
       switch (humanScore > computerScore) {
         case true:
-          console.log(
-            `Congratulations, You win the game!!!! ${humanScore} || Computer: ${computerScore} `,
-          );
-          alert(
-            `Congratulations, You win the game!!!! ${humanScore} || Computer: ${computerScore}`,
-          );
+          alert(`Congratulations, You win the game!!!! ${humanScore} || Computer: ${computerScore}`);
           break;
         case false:
-          console.log(
-            `Damn, You lost the game! Better luck next time Human ${humanScore} || Computer: ${computerScore} `,
-          );
-          alert(
-            `Damn, You lost the game! Better luck next time Human ${humanScore} || Computer: ${computerScore}`,
-          );
+          alert(`Damn, You lost the game! Better luck next time Human ${humanScore} || Computer: ${computerScore}`);
           break;
       }
     }
-
     humanScore = 0;
     computerScore = 0;
     gamesPlayed = 0;
-  } else {
-    PlayRound();
   }
+
+  PlayRound();
 }
 
 PlayGame();
